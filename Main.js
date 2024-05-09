cc24f5c45dcdd6618eeaae694f5c6948ef2e732e
rakan566 <168269485+rakan566@users.noreply.github.com>
2024-05-09 10:06:18 +0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,51 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 100,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".order__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".event__content", {
+  ...scrollRevealOption,
+  duration: 1000,
+})
+function clear(){
+  document.getElementById('num').value='';
+  document.getElementById('n1').value='';
+  document.getElementById('d1').value='';
+  document.getElementById('t1').value='';
+  document.getElementById('e1').value='';
+}
